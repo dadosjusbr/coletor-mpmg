4bda0bd02559a8f9ef1a06c667ebe0641bfa275c
marcos-vas <marcosvaas@gmail.com>
2021-12-13 10:07:27 -0300

    feat: metadados para o indice

---
+++ src/metadado.py
@@ -0,0 +1,27 @@
+from coleta import coleta_pb2 as Coleta
+
+
+def captura(month, year):
+    metadado = Coleta.Metadados()
+    metadado.nao_requer_login = True
+    metadado.nao_requer_captcha = True
+    metadado.acesso = Coleta.Metadados.FormaDeAcesso.AMIGAVEL_PARA_RASPAGEM
+    metadado.extensao = Coleta.Metadados.Extensao.HTML
+    metadado.estritamente_tabular = False
+
+    metadado.tem_matricula = True
+
+    metadado.tem_lotacao = True
+    metadado.tem_cargo = True
+    metadado.receita_base = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+    metadado.despesas = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+    metadado.formato_consistente = True
+    metadado.outras_receitas = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+    """
+    Nessa data ocorre uma mudança nos formatos das planilhas,
+    tanto as de remunerações quando as indenizatorias.
+    """
+    if (year == "2020" and month == "05"):
+        metadado.formato_consistente = False
+        
+    return metadado
